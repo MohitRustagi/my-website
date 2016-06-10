6cf2e4478e46a15996f7e31fbeac5cd3fed3c7fb
MOHIT <mohit.rustagi@live.in>
2016-06-11 00:39:17 +0530

    resume for download

--- routes/urls.js
+++ routes/urls.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home');
 });
+
+/* return my resume */
+router.get('/download-resume/', function(req, res) {
+  res.download('public/MohitRustagi_Resume.pdf');
+});	 
 
 module.exports = router;
