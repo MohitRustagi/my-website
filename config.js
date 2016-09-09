f147fef5a7ca06f36029d12667c25b19158c5db8
MOHIT <mohit.rustagi@live.in>
2016-09-10 01:38:44 +0530

    merged

---
+++ config.js
@@ -0,0 +1,22 @@
+/*************
+*********
+******
+ Configuration
+******
+*********
+**************/
+
+// config = {}
+
+
+// if(process.env.NODE_ENV == "production") {
+// 	config.productId = '57d0668e8db53dfda8a6f099';
+// 	config.wKey = 'A34CB7910F01056189C9FBC2AB0FBA28195A7817A26D3FFC6E35B1F15FF78BD639AF92DB8D3179D0F79441EDB4DDAEE966447CED2BD8064EFDB7BA344AD150B3B37EE22B78ECD1095EFE25DC9D71C90567A76272652DC02B64CDCF78562F373D';
+// } else {
+// 	config.productId = '';
+// 	config.wKey = '';
+// }
+
+
+
+// module.exports = config;
